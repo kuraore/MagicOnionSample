fee3f86978d9024549cd035504fd68f48df35221
kuraore <kuraore11@gmail.com>
2019-04-11 00:43:10 +0900

    サービスを定義

---
+++ MagicOnionSample.ServiceDefinition/IMyFirstService.cs
@@ -0,0 +1,9 @@
+﻿using MagicOnion;
+
+namespace MagicOnionSample.ServiceDefinition
+{
+    public interface IMyFirstService : IService<IMyFirstService>
+    {
+        UnaryResult<int> SumAsync(int x, int y);
+    }
+}
