eb47af06580427a05fb91db2a2f901608790d4b5
kuraore <kuraore11@gmail.com>
2019-04-11 00:47:46 +0900

    サービスを実装

---
+++ MagicOnionSample.Server/MyFirstService.cs
@@ -0,0 +1,18 @@
+﻿using MagicOnion;
+using MagicOnion.Server;
+using MagicOnionSample.ServiceDefinition;
+
+namespace MagicOnionSample.Server
+{
+#pragma warning disable 1998
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+    {
+        public async UnaryResult<int> SumAsync(int x, int y)
+        {
+            Logger.Debug($"Received:{x}, {y}");
+
+            return x + y;
+        }
+    }
+#pragma warning restore 1998
+}
