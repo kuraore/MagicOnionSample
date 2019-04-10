7e04dd04912615269986a2e6a7c2d1a4d34e2cc6
kuraore <kuraore11@gmail.com>
2019-04-11 00:59:56 +0900

    サーバーを実装

--- MagicOnionSample.Server/Program.cs
+++ MagicOnionSample.Server/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Grpc.Core;
+using MagicOnion.Server;
+using System;
 
 namespace MagicOnionSample.Server
 {
@@ -6,7 +8,22 @@ namespace MagicOnionSample.Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+
+            // setup MagicOnion and option.
+            var service = MagicOnionEngine.BuildServerServiceDefinition(isReturnExceptionStackTraceInErrorDetail: true);
+
+            var server = new global::Grpc.Core.Server
+            {
+                Services = { service },
+                Ports = { new ServerPort("localhost", 12345, ServerCredentials.Insecure) }
+            };
+
+            // launch gRPC Server.
+            server.Start();
+
+            // and wait.
+            Console.ReadLine();
         }
     }
 }
